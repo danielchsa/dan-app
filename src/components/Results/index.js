e0bc1e2cbe6b1b0215637298a29d703e343f42f7
danielchsa <dan722sz@gmail.com>
2023-02-17 18:16:40 -0600

    (main): Get products if not in context

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,21 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Context from "../../context/ProductContex";
 
-import useProduct from "../../hooks/useProduct";
+import { getProductsByCategory } from "../../services/getProductByCategory";
 import { ProductList } from "../ProductList";
 
 function Results({params}) {
     const {keyword} = params;
 
-    const {products: productList} = useContext(Context)
-    const products = productList.filter(item => item.category === decodeURI(keyword))  
+    const {products: productList, setProducts} = useContext(Context)
+    const products = productList.filter(item => item.category === decodeURI(keyword))
+
+    useEffect(() => {
+      if(products.length < 1){
+        getProductsByCategory(keyword).then(setProducts).catch(err => console.log(err))
+      }
+
+    }, [keyword])
+    
 
 
-
-    return <>
-        <h3>Searching results for "{decodeURI(keyword)}" </h3>
-        <ProductList products={products} />
-    </>;
+    return  <>
+                <h3>Searching results for "{decodeURI(keyword)}" </h3>
+                <ProductList products={products} />
+            </>;
 }
 
 export default Results;
