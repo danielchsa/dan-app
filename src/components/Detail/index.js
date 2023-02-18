2a646f49344a3f5dac1620a3e9dba90e45ea4b52
danielchsa <dan722sz@gmail.com>
2023-02-17 18:17:20 -0600

    (main): Call service if there is no product

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,40 +1,35 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Context from "../../context/ProductContex";
+import { getSingleProduct } from "../../services/getSingleProduct";
 
-import './Detail.css'
+import { Details } from "./details";
 
 
 function ProductDetail({params}) {
     const { id } = params
     const { products } = useContext(Context)
+    const [localProduct, setLocalProduct] = useState()
 
     const product = products.find(item => item.id === parseInt(id))
-    
+
     useEffect(()=>{
         if(!product){
+            getSingleProduct(id).then(setLocalProduct).catch(err => console.log(err))
             
         }
     },[])
+
     return (
         <>
-        {!product ? <h5>Cargando...</h5>
-        :
-        <>
-            <h1>Detail</h1>
-            <div className="detail">
-                <div className="product-info">
-                    <h5>{product.title}</h5>
-                    <p>{product.description}</p>
-                    <span>${product.price}</span>
-                    <button className="btn">Comprar</button>
-                </div>
-                <div className="product-img">
-                <img src={product.image} alt='item' width='300px' height='300px' />
-                </div>
-            </div> 
-        </>   
-    }
-            
+        {!product ? 
+            (
+                <Details product={localProduct} /> 
+            )
+            :
+            (
+                <Details product={product} />
+            )
+        }
         </>
     )
 }
